911436c1d5d68cee786dda1f996e6d1833d33ead
enockabere <maebaenock95@gmail.com>
2022-06-30 00:58:24 +0300

    changes

--- KMPDC/settings.py
+++ KMPDC/settings.py
@@ -179,10 +179,10 @@ django_heroku.settings(locals())
 
 AUTHS = Session()
 
-WEB_SERVICE_PWD = 'Password@123'
+WEB_SERVICE_PWD = 'Admin@123'
 BASE_URL = 'http://20.121.189.145:7047/KMPDC/WS/KMPDC/Codeunit/CuRecruitmentWebPortal'
 O_DATA = "http://20.121.189.145:7048/KMPDC/ODataV4/Company(%27KMPDC%27){}"
-AUTHS.auth = HTTPBasicAuth('WINNIE', WEB_SERVICE_PWD)
+AUTHS.auth = HTTPBasicAuth('EMAEBA', WEB_SERVICE_PWD)
 
 CLIENT = Client(BASE_URL, transport=Transport(session=AUTHS))
-AUTHS = HTTPBasicAuth('WINNIE', WEB_SERVICE_PWD)
+AUTHS = HTTPBasicAuth('EMAEBA', WEB_SERVICE_PWD)
